f8480eee53f2fb3d6f406568836dd1b8ec33707f
Oleg Malinskiy <malinskii_o@tass.ru>
2020-10-05 22:16:23 +0300

    Rewrite header's validation

--- src/Headers.php
+++ src/Headers.php
@@ -22,7 +22,7 @@ class Headers
      */
     public function hasHeader(string $name): bool
     {
-        $normalizedName = $this->normalizeName($name);
+        $normalizedName = $this->preprareAndNormalizeName($name);
         return !empty($this->normalizedMap[$normalizedName]);
     }
 
@@ -40,7 +40,7 @@ class Headers
      */
     public function getHeader(string $name): array
     {
-        $normalizedName = $this->normalizeName($name);
+        $normalizedName = $this->preprareAndNormalizeName($name);
         if (!empty($this->normalizedMap[$normalizedName])) {
             $name = $this->normalizedMap[$normalizedName];
             return $this->originalHeaders[$name];
@@ -54,7 +54,7 @@ class Headers
      */
     public function getHeaderLine(string $name): string
     {
-        $normalizedName = $this->normalizeName($name);
+        $normalizedName = $this->preprareAndNormalizeName($name);
         if (!empty($this->normalizedMap[$normalizedName])) {
             $name = $this->normalizedMap[$normalizedName];
             return implode(',', $this->originalHeaders[$name]);
@@ -69,8 +69,7 @@ class Headers
      */
     public function addHeader(string $name, $value, bool $rewrite = true): void
     {
-        $this->validateHeaderName($name);
-        $this->validateHeaderValue($value);
+        $this->validateHeader($name, $value);
 
         $name = $this->prepareName($name);
         $value = $this->preprareValue($value);
@@ -99,7 +98,7 @@ class Headers
      */
     public function removeHeader(string $name): void
     {
-        $normalizedName = $this->normalizeName($name);
+        $normalizedName = $this->preprareAndNormalizeName($name);
         if (!empty($this->normalizedMap[$normalizedName])) {
             $name = $this->normalizedMap[$normalizedName];
             unset($this->originalHeaders[$name]);
@@ -109,34 +108,10 @@ class Headers
 
     /**
      * @param string $name
-     */
-    protected function validateHeaderName(string $name): void
-    {
-        if (empty($name)) {
-            throw new InvalidArgumentException('Name of header must be not empty.');
-        }
-    }
-
-    /**
-     * @param $value
-     */
-    protected function validateHeaderValue($value): void
-    {
-        if (!is_string($value) && !is_array($value)) {
-            throw new InvalidArgumentException('Value of header must be a string or an array.');
-        }
-        if (empty($value)) {
-            throw new InvalidArgumentException('Value of header must be not empty.');
-        }
-    }
-
-    /**
-     * @param string $name
      * @return string
      */
     protected function normalizeName(string $name): string
     {
-        $name = $this->prepareName($name);
         return strtolower($name);
     }
 
@@ -148,16 +123,67 @@ class Headers
     {
         return str_replace('_', '-', $name);
     }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function preprareAndNormalizeName(string $string): string
+    {
+        $name = $this->prepareName($string);
+        return $this->normalizeName($name);
+    }
+
     /**
      * @param string|array $value
      * @return array
      */
     protected function preprareValue($value): array
     {
-        if (is_string($value)) {
-            return [trim($value)];
-        } else {
-            return array_map(fn($item) => trim($item), $value);
+        return is_string($value) ? [trim($value)] : array_map(fn($item) => trim($item), $value);
+    }
+
+    /**
+     * @param string $name
+     * @param string|array $value
+     */
+    protected function validateHeader(string $name, $value): void
+    {
+        $this->validateHeaderName($name);
+        $this->validateHeaderValue($value);
+    }
+
+    /**
+     * @param string $name
+     */
+    protected function validateHeaderName(string $name): void
+    {
+        if (!is_string($name) || preg_match("@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@", $name) !== 1) {
+            throw new InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
+        }
+    }
+
+    /**
+     * @param string|array $value
+     */
+    protected function validateHeaderValue($value): void
+    {
+        if (!is_string($value) && !is_array($value)) {
+            throw new InvalidArgumentException('Value of header must be a string or an array.');
+        }
+        if (empty($value)) {
+            throw new InvalidArgumentException('Value of header must be not empty.');
+        }
+
+        $items = is_string($value) ? [trim($value)] : array_map(fn($item) => trim($item), $value);
+
+        $pattern = "@^[ \t\x21-\x7E\x80-\xFF]*$@";
+        foreach ($items as $item) {
+            $hasInvalidType = !is_numeric($item) && !is_string($item);
+            $rejected = $hasInvalidType || preg_match($pattern, (string) $item) !== 1;
+            if ($rejected) {
+                throw new InvalidArgumentException('Header values must be RFC 7230 compatible strings.');
+            }
         }
     }
 }
